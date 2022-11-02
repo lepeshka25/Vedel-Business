fb7749942e78d21e5572066673ce3189324db22f
lepeshka25 <ruslanmuratbekov1972@gmail.com>
2022-11-02 16:03:09 +0600

    finish frameSix

--- src/App/Layout/Main/components/Frame_6/index.tsx
+++ src/App/Layout/Main/components/Frame_6/index.tsx
@@ -1,9 +1,14 @@
 import React, {FC} from 'react';
+import background from '../../../../../assets/Main/FrameSix/Mask Group.png'
+import cs from './style.module.scss'
 
 const FrameSix: FC = () => {
 	return (
-		<div>
-
+		<div style={{background: `url("${background}") center / cover no-repeat`}} className={cs.frameSix}>
+			<h1>Начните менять свою жизнь уже сегодня</h1>
+			<button>
+				зарегистрироваться
+			</button>
 		</div>
 	);
 };
